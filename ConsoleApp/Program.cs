cfbd9cffa7f767ad8f0560e382da95f634a6e7b7
sanek2bas <sanek_2bas@mail.ru>
2022-05-28 17:40:36 +0300

    added input processing + first problem

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System; 
 
 namespace ConsoleApp
 {
@@ -6,8 +6,35 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            while (true)
+            {
+                Console.Write("Enter problem number or exit to exit program: ");
+                var numberStr = Console.ReadLine();
+                if (numberStr == "exit")
+                    break;
+                int number = 0;
+                try
+                {
+                    number = Convert.ToInt32(numberStr);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+
+                int result;
+                switch (number)
+                {
+                    case 1:
+                        result = Problem_1.GetResult;
+                        break;
+                    default:
+                        Console.WriteLine("There is no problem with this number");
+                        continue;
+                }
+                Console.WriteLine(result);
+            }
         }
     }
 }
---
+++ ProblemSolutions/Problem_1.fs
@@ -0,0 +1,13 @@
+﻿module Problem_1
+    
+let private sumDevidableByX (x : int, limit : int) =
+    let mutable sum = 0
+    for i in x .. x .. limit do
+        sum <- sum + i
+    sum
+
+let public GetResult =
+    let limit = 999;
+    let three = 3;
+    let five = 5;
+    sumDevidableByX(three, 1000) + sumDevidableByX(five, limit) - sumDevidableByX(three * five, limit)
